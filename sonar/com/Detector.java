09729e58573bf9f9d25e8fa455a9b63d5343361d
Cisnake <35097455+Cisnake@users.noreply.github.com>
2018-03-06 14:23:11 +0000

    Add Project

---
+++ sonar/com/Detector.java
@@ -0,0 +1,38 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Detector {
+  public static void main(String[] args) throws InterruptedException {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      System.out.println("#==> Can not find JDBC driver for MySQL.");
+      e.printStackTrace();
+      System.exit(1);
+    }
+    String url = "jdbc:mysql://a_mysql:3306/sonar";
+    String user = "sonar";
+    String password = "sonar";
+    int retries = 120;
+    long interval = 5000;
+    Connection connection;
+    for (int i = 0; i < retries; i++) {
+      try {
+        connection = DriverManager.getConnection(url, user, password);
+        if (connection != null) {
+          System.out.println("#==> DB connection is successful.");
+          return;
+        }
+		
+      } catch (SQLException e) {
+        System.out.println("#==> Can not establish a connection to the DB.");
+      }
+      Thread.sleep(interval);
+    }
+    System.out.println("#==> Failed to connect to the DB.  Quit.");
+    System.exit(1);
+  }
+}
